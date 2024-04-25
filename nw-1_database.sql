d1e58fbe0f820c3449f2d8d2e3b74a0da3f3755a
TanedThanyapanich <97682105+KimJiWon123@users.noreply.github.com>
2024-04-25 21:04:58 +0700

    Update nw-1_database.sql

--- nw-1_database.sql
+++ nw-1_database.sql
@@ -25,4 +25,4 @@ INSERT INTO tasks (taskId, taskTitle, taskDescription, taskAssignees, taskStatus
 VALUES (3, 'ดาต้าเบส', 'ສ້າງຖານຂໍ້ມູນ', 'あなた、彼、彼女 (私ではありません)', 'Doing', '2024-04-22 09:10:00', '2024-04-25 00:00:00');
 
 INSERT INTO tasks (taskId, taskTitle, taskDescription, taskAssignees, taskStatus, createdOn, updatedOn)
-VALUES (4, 'Infrastructure', 'Setup containers', 'ไก่งวง กับ เพนกวิน', 'Done', '2024-04-22 09:15:00', '2024-04-22 10:00:00');
+VALUES (4, '_Infrastructure_', 'Setup containers', 'ไก่งวง กับ เพนกวิน', 'Done', '2024-04-22 09:15:00', '2024-04-22 10:00:00');
